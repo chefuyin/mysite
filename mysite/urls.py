6ec79bd3a495d2028c2852d01e60730832f4ecc0
chefuyin <chefuyin@foxmail.com>
2018-03-01 18:07:31 +0800

    add some examples

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
- # include function allows referencing other URLconfs
+from django.urls import path 
+from law import views as law_views
+
 urlpatterns = [
-    path('law/', include('law.urls')), # add the app
+    path('', law_views.index), # add the app
     path('admin/', admin.site.urls),
+    path('home/',law_views.home,name='home'),
+    path('index/',law_views.index,name='index'),
 ]
