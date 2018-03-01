4ee9283814c8a4347b6cddb55bb093cdda959e39
chefuyin <chefuyin@foxmail.com>
2018-03-01 18:10:29 +0800

    add some examples again

--- law/views.py
+++ law/views.py
@@ -3,5 +3,17 @@ from django.http import HttpResponse
 import time
 
 # Create your views here.
+def home(request):
+    # return render(request,'home.html')
+    string = "django learning"
+    tlist = [i for i in range (1,10)]
+    info_dict={'site':'lawtribes','content':'law search'}
+    List = map(str,range(20))
+
+    return render(request,'home.html',
+    {'string':string,'tlist':tlist,
+    'info_dict':info_dict,'List':List})
+
+
 def index(request):
     return HttpResponse("HELLO,WORLD!YOU'RE AT THE INDEX.IT'S {}".format(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())))
